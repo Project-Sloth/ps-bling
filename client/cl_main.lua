8e34e610abaf89b46f3c6982daa442221ac39763
MonkeyWhisper <82112471+MonkeyWhisper@users.noreply.github.com>
2023-03-25 17:49:48 -0400

    Better handling of events and player states

--- client/cl_main.lua
+++ client/cl_main.lua
@@ -1,41 +1,28 @@
 CreateThread(function()
-	while true do
-		local ped = PlayerPedId()
-		local coords = GetEntityCoords(ped)
-		local chain = GetPedDrawableVariation(ped, 7)
-		if Config.Chains[chain] then
-			TriggerServerEvent("ps-bling:server:effects", PedToNet(ped), coords)
-		end
-		Wait(math.random(Config.BlingTime.Min, Config.BlingTime.Max) * 1000)
-	end
-end)
+    while true do
+        local playerPed = PlayerPedId()
+        local coords = GetEntityCoords(playerPed)
+        local chain = GetPedDrawableVariation(playerPed, 7)
 
-RegisterNetEvent("ps-bling:client:effects", function(ped, coords)
-	local distance = #(GetEntityCoords(PlayerPedId()) - coords)
-	ped = NetToPed(ped)
-	if distance <= 300 then
-		if DoesEntityExist(ped) and not IsEntityDead(ped) then
-			Bling = UseParticleFxAssetNextCall("core")
-			Particle = StartParticleFxLoopedOnEntityBone("fire_wrecked_plane_cockpit", ped, 0.35, 0.25, 0.0, 0.0, 0.0, 0.0, GetPedBoneIndex(ped, 23553), 0.01, 0.0, 0.0, 0.0)
-			Wait(2800)
-			while DoesParticleFxLoopedExist(Bling) do
-				StopParticleFxLooped(Bling, 1)
-				Wait(0)
-			end
-			while DoesParticleFxLoopedExist(Particle) do
-				StopParticleFxLooped(Particle, 1)
-				Wait(0)
-			end
-			while DoesParticleFxLoopedExist("fire_wrecked_plane_cockpit") do
-				StopParticleFxLooped("fire_wrecked_plane_cockpit", 1)
-				Wait(0)
-			end
-			while DoesParticleFxLoopedExist("core" ) do
-				StopParticleFxLooped("core" , 1)
-				Wait(0)
-			end
-			Wait(2800*3)
-			RemoveParticleFxFromEntity(ped)
-		end
-	end
+        if Config.Chains[chain] then
+            TriggerServerEvent("ps-bling:server:effects", PedToNet(playerPed), coords)
+        end
+
+        Wait(math.random(Config.BlingTime.Min, Config.BlingTime.Max) * 1000)
+
+        local distance = #(GetEntityCoords(playerPed) - coords)
+        if distance <= 300 then
+            if DoesEntityExist(playerPed) and not IsEntityDead(playerPed) then
+                Bling = UseParticleFxAssetNextCall("core")
+                Particle = StartParticleFxLoopedOnEntityBone("fire_wrecked_plane_cockpit", playerPed, 0.35, 0.25, 0.0, 0.0, 0.0, 0.0, GetPedBoneIndex(playerPed, 23553), 0.01, 0.0, 0.0, 0.0)
+                Wait(2800)
+                StopParticleFxLooped(Bling, 1)
+                StopParticleFxLooped(Particle, 1)
+                StopParticleFxLooped("fire_wrecked_plane_cockpit", 1)
+                StopParticleFxLooped("core", 1)
+                Wait(2800 * 3)
+                RemoveParticleFxFromEntity(playerPed)
+            end
+        end
+    end
 end)
--- server/sv_main.lua
+++ server/sv_main.lua
@@ -1,12 +1,27 @@
 local QBCore = exports['qb-core']:GetCoreObject()
 
+local function updatePlayerPosition(playerId)
+    local ped = GetPlayerPed(playerId)
+    local coords = GetEntityCoords(ped)
+    local playerState = Player(playerId).state
+
+    playerState:set("ps-bling:coords", {
+        x = coords.x,
+        y = coords.y,
+        z = coords.z
+    }, true)
+end
+
 RegisterNetEvent("ps-bling:server:effects", function(entity, coords)
-	for _, player in pairs(QBCore.Functions.GetPlayers()) do
-        local ped = GetPlayerPed(player)
-        local pcoords = GetEntityCoords(ped)
-        local dist = #(pcoords - coords)
-        if dist <= 300 then
-            TriggerClientEvent("ps-bling:client:effects", player, entity, coords)
+    for _, player in pairs(QBCore.Functions.GetPlayers()) do
+        local playerState = Player(player).state
+        local pcoords = QBCore.Functions.GetCoords(ped)
+
+        if pcoords then
+            local dist = #(vector3(pcoords.x, pcoords.y, pcoords.z) - coords)
+            if dist <= 300 then
+                TriggerClientEvent("ps-bling:client:effects", player, entity, coords)
+            end
         end
     end
 end)
